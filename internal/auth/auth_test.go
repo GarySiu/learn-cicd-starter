7ea04c580cf79751bc29de12fb9d49d14a4c29ab
Gary Siu <gary@untypeable.com>
2026-05-13 08:38:59 +0100

    push deliberately broken test

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,23 @@
+package auth
+
+import (
+	"testing"
+	"net/http"
+)
+
+func TestNoAuthHeader(t *testing.T) {
+	var h http.Header
+	_, err := GetAPIKey(h)
+	if err != ErrNoAuthHeaderIncluded {
+		t.Errorf("incorrect error: %v", err)
+	}
+}
+
+func TestGetAPIKey(t *testing.T) {
+	h := http.Header{}
+	h.Set("Authorization", "ApiKey abc123")
+	got, _ := GetAPIKey(h)
+	if got != "abc1234" {
+		t.Errorf("unexpected api value: %v", got)
+	}
+}
